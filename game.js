15548ef08f9f72f7d883cad310602afec6364ad4
J. Hassler Thurston <jthurst3@u.rochester.edu>
2015-01-04 20:12:15 -0500

    put fixing functionality in game.js, add fixOval and fixSquiggle

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-// alert("Hello");
+// correcting the width and height of the diamond
+var fixDiamond = function(id) {
+	$("#"+id + " .triangle-up").css("border-bottom-width", $("#"+id).height()/2);
+	$("#"+id + " .triangle-down").css("border-top-width", $("#"+id).height()/2);
+	$("#"+id + " .triangle-up").css("border-left-width", $("#"+id).width()/2);
+	$("#"+id + " .triangle-up").css("border-right-width", $("#"+id).width()/2);
+	$("#"+id + " .triangle-down").css("border-left-width", $("#"+id).width()/2);
+	$("#"+id + " .triangle-down").css("border-right-width", $("#"+id).width()/2);
+};
+// correcting the width and height of the oval
+var fixOval = function(id) {
+	$("#"+id + " .oval").css("width", $("#"+id).width());
+	$("#"+id + " .oval").css("height", $("#"+id).height());
+};
+// correcting the width and height of the squiggle
+var fixSquiggle = function(id) {
+	$("#"+id + " .curve-1").css("width", $("#"+id).width()/2);
+	$("#"+id + " .curve-1").css("height", $("#"+id).height()/2);
+	$("#"+id + " .curve-2").css("width", $("#"+id).width()/2);
+	$("#"+id + " .curve-2").css("height", $("#"+id).height()/2);
+};
